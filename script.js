a23430b15eab3ea7b6176ea47586956770e5096b
PallaviThotakuri <160385755+PallaviThotakuri@users.noreply.github.com>
2024-02-26 19:35:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function addTask() {
+    var taskInput = document.getElementById("taskInput");
+    var taskList = document.getElementById("taskList");
+
+    if (taskInput.value.trim() !== "") {
+        var li = document.createElement("li");
+        li.textContent = taskInput.value;
+
+        var deleteButton = document.createElement("span");
+        deleteButton.textContent = " ❌";
+        deleteButton.className = "delete";
+        deleteButton.onclick = function() {
+            taskList.removeChild(li);
+        };
+
+        li.appendChild(deleteButton);
+        taskList.appendChild(li);
+
+        taskInput.value = "";
+    } else {
+        alert("Please enter a task!");
+    }
+}
